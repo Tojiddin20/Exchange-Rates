65e3c1ae82056758582acfaaa8b0b1545d3f3dc1
Tojiddin Khamidov <khamidovtojiddin20@gmail.com>
2022-06-21 09:37:40 +0500

    Added new seeder - ArchiveRates

--- database/seeders/ArchiveRates.php
+++ database/seeders/ArchiveRates.php
@@ -8,33 +8,45 @@ use Illuminate\Database\Seeder;
 use App\Models\ExchangeRate;
 use App\Services\ExchangeRateGetter;
 
-class DatabaseSeeder extends Seeder
+class ArchiveRates extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
-    
+
     public function __construct(
         private ExchangeRateGetter $exchangeRateGetter
     ) {}
 
     public function run()
     {
-
-    // RECEIVE AND SEED FRESH EXCHANGE RATES
-        $currencies = $this->exchangeRateGetter->get();
-
-        foreach ($currencies as $currency) {
+        // ARCHIVE EXCHANGE RATES    
+        $archiveCurrencies = $this->exchangeRateGetter->getArchives();
+        
+        $month = 6;
+        $day = 10;
+        $calculate = 0;
+        foreach ($archiveCurrencies as $currency) {
             ExchangeRate::insert([
-                "date" => date("Y-m-d"),
+                "date" => '2022-'.$month.'-'.$day,
                 "currency_from" => $currency['currency_from'],
                 "currency_to" => $currency['currency_to'],
                 "nominal" => $currency['nominal'],
                 "value" => $currency['value']
             ]);
-        }
+            
+            $calculate++;
 
+            if($calculate == 5) {
+                $day--;
+                $calculate = 0;
+            }
+            if($day == 0) {
+                $month = 5;
+                $day = 31;
+            }
+        }
     }
 }
